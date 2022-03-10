a5f27a406728594a0505c22cd5dab8b5593db8d6
Dirk Habighorst <dirk.habighorst@googlemail.com>
2022-03-10 09:53:57 +0100

    do not publish task result

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/er_worker.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/er_worker.py
@@ -7,7 +7,7 @@ app = Celery("worker", broker="amqp://guest@rabbitmq//", result_backend = 'rpc:/
 nlp = spacy.load("en_core_web_md")
 
 
-@app.task
+@app.task(ignore_result=True)
 def add_entities(content: str):
     doc = nlp(content)
     documents.add_entities(content, doc.ents)
